9aa174f1f170487fe210c3ddcce5fa63f1653ab8
achmad habibi <habiby.ach@gmail.com>
2020-03-24 17:40:04 +0800

    BUat object scanner

--- InputOutput.java
+++ InputOutput.java
@@ -1,4 +1,7 @@
+import java.until.Scanner;
+
 public class InputOutout{
-public static void main(String [] args){
+ public static void main(String [] args){
+ Scanner scanner = new Scanner(System.in);
  }
 }
